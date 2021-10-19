b4ca5c94e8554dc7f35a62f6277319c5ca69d1f6
Louis-Philippe <louisphilippe.turmel@gmail.com>
2021-10-19 08:58:02 -0400

    Fixed table display in dissertation-critique.md

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,26 +5,27 @@ import { Fragment, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
 export default function Home({ posts }) {
-    const [visible, setVisible] = useState(true);
+	const [visible, setVisible] = useState(true);
 
-    return (
-        <div className="container mx-auto max-w-md">
-            <motion.p
-                initial={{ opacity: 0, top: "-100px" }}
-                animate={{
-                    scale: 1,
-                    opacity: 1,
-                    top: 0,
-                    position: "relative",
-                    transition: {
-                        delay: 0.3,
-                    },
-                }}
-                className="font-bold text-4xl mb-10 absolute text-center mt-16"
-            >
-                OS Documentation
-            </motion.p>
-            {/* <div>
+	return (
+		<div className="container mx-auto max-w-md">
+			<motion.p
+				initial={{ opacity: 0, top: "-100px" }}
+				animate={{
+					scale: 1,
+					opacity: 1,
+					top: 0,
+					position: "relative",
+					transition: {
+						delay: 0.3,
+					},
+				}}
+				className="font-bold text-4xl mb-10 absolute text-center mt-16"
+			>
+				OS Documentation
+			</motion.p>
+
+			{/* <div>
                 <button onClick={() => setVisible(!visible)}> Do it </button>
                 <AnimatePresence>
                     {visible && (
@@ -37,33 +38,33 @@ export default function Home({ posts }) {
                     )}
                 </AnimatePresence>
             </div> */}
-            {posts.map((post, index) => (
-                <Fragment key={shortId.generate()}>
-                    <PostItem post={post.data} slug={post.slug} index={index} />
-                </Fragment>
-            ))}
-        </div>
-    );
+			{posts.map((post, index) => (
+				<Fragment key={shortId.generate()}>
+					<PostItem post={post.data} slug={post.slug} index={index} />
+				</Fragment>
+			))}
+		</div>
+	);
 }
 
 export async function getStaticProps(context) {
-    var posts = getPosts();
-    posts = posts.map((post) => {
-        const languages = post.data.languages
-            ? post.data.languages.split(",")
-            : [];
-        return {
-            data: {
-                ...post.data,
-                languages,
-            },
-            content: post.content,
-            slug: post.slug,
-        };
-    });
-    return {
-        props: {
-            posts,
-        },
-    };
+	var posts = getPosts();
+	posts = posts.map((post) => {
+		const languages = post.data.languages
+			? post.data.languages.split(",")
+			: [];
+		return {
+			data: {
+				...post.data,
+				languages,
+			},
+			content: post.content,
+			slug: post.slug,
+		};
+	});
+	return {
+		props: {
+			posts,
+		},
+	};
 }
